c7f1d7ef9963a4aabcaf30a82b6a80b2cb4db3bc
w0jtaz <wojtaz@vp.pl>
2021-03-08 12:59:03 +0100

    debug2

---
+++ cw2/debug2/main.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //wpisz wartosci zmiennych po wykonaniu danej linijki kodu
+    int a = 162436; //tu wpisz swoj numer indeksu
+    int b=a%12;
+    int c=b%7;
+    int d= (b+=4)-(c*=2); // b=8  , c= 8 , d=0
+    c=(d^=2); // b=8  , c=2  , d=2
+    d=(b/=4)*(c-=3); // b=2 , c=-1  , d=-2
+    return 0;
+}
