578e69e47385f65a825e681c5852e148b3ef7f8b
w0jtaz <wojtaz@vp.pl>
2021-03-21 15:21:48 +0100

    cw4

--- cw4/3.2.4/main.c
+++ cw4/3.2.4/main.c
@@ -5,14 +5,26 @@ int* foo(int *a, int *b)
 {
     if (*a>*b)
     {
-        return *b;
+        int temp=*a;
+        *a=*b;
+        *b=temp;
     }
-    return *a;
 }
 
 int main()
 {
-    int x = 66, y=7;
-    printf("%d\n", foo(&x,&y));
+    //przypadek 1
+    int x=6,y=9;
+    printf("%d %d\n",x,y);
+    foo(&x,&y);
+    printf("%d %d\n",x,y);
+
+    printf("\n");
+    //przypadek 2
+    int z=6,m=3;
+    printf("%d %d\n",z,m);
+    foo(&z,&m);
+    printf("%d %d\n",z,m);
+
     return 0;
 }
