c9148c4f6c576262407a37db3a2e8473b15faab1
w0jtaz <wojtaz@vp.pl>
2021-03-29 14:15:24 +0200

    cw6

--- cw6/4.2.2/main.c
+++ cw6/4.2.2/main.c
@@ -10,7 +10,7 @@ void wyswietl(int n, int tab[n])
     printf("\n");
 }
 
-int fooA(int n, int tab[n])
+float fooA(int n, int tab[n])
 {
     int temp = 0;
     for(int i=0; i<n; i++)
@@ -42,10 +42,10 @@ int fooC(int n, int tab[n])
 
 int main()
 {
-    int tab1[4] = {6,2,3,4,5};
-    wyswietl(4,tab1);
-    printf("%d\n", fooB(4, tab1));
-    printf("%float\n", fooA(4, tab1));
+    int tab1[5] = {6,2,3,4,5};
+    wyswietl(5,tab1);
+    printf("%d\n", fooB(5, tab1));
+    printf("%f\n", fooA(5, tab1));
     printf("%d\n", fooC(5, tab1));
 
     return 0;
