d4bf41c83200b3376f482dec2f7db02863a4ec76
w0jtaz <wojtaz@vp.pl>
2021-04-11 21:37:59 +0200

    cw6

--- cw6/4.2.7/main.c
+++ cw6/4.2.7/main.c
@@ -19,6 +19,37 @@ int fooA(int n, int tab1[n], int tab2[n], int tab3[n])
     return tab3[n];
 }
 
+int fooB(int n, int tab4[n], int tab5[n], int tab6[n])
+{
+    for(int i=0; i<n; i++)
+    {
+        if(tab4[i]>tab5[i])
+        {
+            tab6[i] += tab4[i];
+        }
+        else
+        {
+            tab6[i] += tab5[i];
+        }
+    }
+    return tab6[n];
+}
+
+int fooC(int n, int tab7[n], int tab8[n], int tab9[n])
+{
+    int pom7[n]; int pom8[n]; int pom9[n];
+    for(int i=0; i<n; i++)
+    {
+        tab7[i] = pom7[i];
+        tab8[i] = pom8[i];
+        tab9[i] = pom9[i];
+        tab8[i] = tab7[i];
+        tab9[i] = pom8[i];
+        tab7[i] = pom9[i];
+    }
+    return tab7[n];tab8[n];tab9[n];
+}
+
 int main()
 {
     int tab1[5] = {1,2,3,4,5};
@@ -28,7 +59,31 @@ int main()
     wyswietl(5,tab2);
     wyswietl(5,tab3);
     fooA(5, tab1, tab2, tab3);
-    printf("\n");
     wyswietl(5,tab3);
+    printf("\n");
+
+    int tab4[5] = {1,8,3,2,15};
+    int tab5[5] = {2,4,13,8,10};
+    int tab6[5] = {0,0,0,0,0};
+    wyswietl(5,tab4);
+    wyswietl(5,tab5);
+    wyswietl(5,tab6);
+    fooB(5, tab4, tab5, tab6);
+    wyswietl(5,tab6);
+    printf("\n");
+
+    int tab7[5] = {1,2,3,4,5};
+    int tab8[5] = {2,4,6,8,10};
+    int tab9[5] = {0,0,0,0,0};
+    wyswietl(5,tab7);
+    wyswietl(5,tab8);
+    wyswietl(5,tab9);
+    fooC(5, tab7, tab8, tab9);
+    printf("\n");
+    wyswietl(5,tab7);
+    wyswietl(5,tab8);
+    wyswietl(5,tab9);
+    //fooC nie dzia³a jak nale¿y, do poprawy
+
     return 0;
 }
