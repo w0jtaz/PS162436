d6d9c1559491144bb255ed7945a8fcaaaac8a3a2
w0jtaz <wojtaz@vp.pl>
2021-04-11 23:21:38 +0200

    cw6

--- cw6/4.2.12/main.c
+++ cw6/4.2.12/main.c
@@ -21,11 +21,38 @@ int fooA(int n, int tab[n])
     }
 }
 
+int fooB(int n, int tab[n])
+{
+    int temp = tab[0];
+    for(int i=0; i<n-1; i++)
+    {
+        tab[i] = tab[i+1];
+        tab[n-1]=temp;
+    }
+}
+
+int fooC(int n, int tab[n])
+{
+    int temp = tab[n-1];
+    for(int i=n-2; i>=0; i--)
+    {
+        tab[i+1]= tab[i];
+        tab[0]=temp;
+    }
+}
+
 int main()
 {
     int tab[5] = {1,2,3,4,5};
     wyswietl(5, tab);
+    printf("\n");
     fooA(5, tab);
+    wyswietl(5, tab);
+    printf("\n");
+    fooB(5, tab);
+    wyswietl(5, tab);
+    printf("\n");
+    fooC(5, tab);
     wyswietl(5, tab);
 
     return 0;
