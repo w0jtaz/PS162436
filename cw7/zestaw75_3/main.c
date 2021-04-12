634fbea8ae9efc4fcc49da6c7be13d1f51804499
w0jtaz <wojtaz@vp.pl>
2021-04-12 14:12:28 +0200

    cw7 zestaw75

--- cw7/zestaw75_3/main.c
+++ cw7/zestaw75_3/main.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+int foo(double (*fun1)(double a), double(*fun2)(double b), double x)
+{
+    if (x<1)
+        return -1;
+    double n=1;
+    for(double k=1;k<=x;k++)
+    {
+        n=k;
+    }
+    for(int i=1;i<=n;i++)
+    {
+        if(fabs(fun1(x)-fun2(x)*fun2(x)*fun2(x))>0.00001)
+            return -1;
+    }
+    return 1;
+}
+
+double test1(double a)
+{
+    return a;
+}
+
+double test2(double a)
+{
+    return a*a*a;
+}
 
 int main()
 {
-    printf("Hello world!\n");
+    printf("%d\n",foo(test1, test2,5));
+    printf("%d\n",foo(test1, test2,1.3));
+    printf("%d\n",foo(test2, test1,5.2));
+
     return 0;
 }
---
+++ cw7/zestaw75_4/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int foo(int n)
+{
+
+}
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
