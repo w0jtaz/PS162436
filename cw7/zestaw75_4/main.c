4ef86aa8e62bbb93a4f790b8b64cf973dc3a6b08
w0jtaz <wojtaz@vp.pl>
2021-04-18 21:19:26 +0200

    cw7 prawie skończone bez zad 5

--- cw7/zestaw75_4/main.c
+++ cw7/zestaw75_4/main.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int foo(int n)
+int funkcja(int k)
 {
-
+    if (k==0)
+    {
+        return 1;
+    }
+    if (k==1)
+    {
+        return 2;
+    }
+    if (k%2==0)
+    {
+        return funkcja(k-2)+k;
+    }
+    if (k%2!=0)
+    {
+        return funkcja(k-2)*k;
+    }
 }
 
 int main()
 {
-    printf("Hello world!\n");
+    printf("%d\n",funkcja(1));
+    printf("%d\n",funkcja(3));
     return 0;
 }
