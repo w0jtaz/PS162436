f82ad888b2fdc6c4fa1224ce3b0e27224eedf7af
w0jtaz <wojtaz@vp.pl>
2021-04-19 14:09:35 +0200

    cw8

--- cw8/5.2.5/main.c
+++ cw8/5.2.5/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void przepisz(wchar_t *napis1, wchar_t *napis2)
+void przepisz1(char *napis1, char *napis2)
 {
     int i;
     for(i=0; napis1[i]!=0;i++)
@@ -11,15 +11,33 @@ void przepisz(wchar_t *napis1, wchar_t *napis2)
     napis2[i]=0;
 }
 
+void przepisz2(wchar_t *napis3, wchar_t *napis4)
+{
+    int i;
+    for(i=0; napis3[i]!=0;i++)
+    {
+        napis4[i]=napis3[i];
+    }
+    napis4[i]=0;
+}
+
 int main()
 {
-    wchar_t napis1[] =L"napis1";
-    wchar_t napis2[] =L"napis2";
-    wprintf(L"%s\n", napis1);
-    wprintf(L"%s\n", napis2);
+    char napis1[] ="napis1";
+    char napis2[] ="napis2";
+    printf("%s\n", napis1);
+    printf("%s\n", napis2);
+    przepisz1(napis1, napis2);
+    printf("%s\n", napis1);
+    printf("%s\n", napis2);
+
     printf("\n");
-    przepisz(napis1, napis2);
-    wprintf(L"%s\n", napis1);
-    wprintf(L"%s\n", napis2);
+    wchar_t napis3[] =L"napis3";
+    wchar_t napis4[] =L"napis4";
+    wprintf(L"%s\n", napis3);
+    wprintf(L"%s\n", napis4);
+    przepisz2(napis3, napis4);
+    wprintf(L"%s\n", napis3);
+    wprintf(L"%s\n", napis4);
     return 0;
 }
