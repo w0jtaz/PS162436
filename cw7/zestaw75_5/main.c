51b6cebd781c8db5dd44f53216548e83413a0f12
w0jtaz <wojtaz@vp.pl>
2021-04-25 20:42:28 +0200

    cw7 poprawione

--- cw7/zestaw75_5/main.c
+++ cw7/zestaw75_5/main.c
@@ -10,22 +10,41 @@ void wyswietl(int n, int tab[n])
     printf("\n");
 }
 
-int foo(int n, int tab1[n], int tab2[n])
+void sortuj(int n, int tab[])
 {
-    int tab3[2*n];
-    for(int i=0; i<n; i++)
+    for(int i=0;i<n;i++)
     {
-        tab3[i] += tab1[i];
-        tab3[i+1] += tab2[i];
+        for(int j=0;j<n-1;j++)
+        {
+            if (tab[j]<tab[j+1])
+            {
+                int temp=tab[j];
+                tab[j]=tab[j+1];
+                tab[j+1]=temp;
+            }
+        }
     }
-    return *tab3;
+}
+
+int *foo(int n, int tab1[], int tab2[])
+{
+    int *temp=malloc(2*n*sizeof(n));
+    for(int i=0;i<n;i++)
+    {
+        temp[i]=tab1[i];
+        temp[i+n]=tab2[i];
+    }
+    sortuj(2*n,temp);
+    return temp;
 }
 
 int main()
 {
     int tab1[5] = {5,1,3,4,2};
     int tab2[5] = {-2,-7,0,9,2};
-    foo(5, tab1, tab2);
-    wyswietl(5, foo(5, tab1, tab2));
+    wyswietl(5, tab1);
+    wyswietl(5, tab2);
+    int *tablica_posortowana = foo(5, tab1, tab2);
+    wyswietl(10, tablica_posortowana);
     return 0;
 }
