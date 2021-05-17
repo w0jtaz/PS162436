54d267037588321a872eab21280805a2832934de
w0jtaz <wojtaz@vp.pl>
2021-05-17 14:31:13 +0200

    cw11

--- cw11/7.2.7/main.c
+++ cw11/7.2.7/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 struct zespolone
 {
@@ -14,12 +15,30 @@ struct zespolone dodaj(struct zespolone z1, struct zespolone z2)
     temp.im=z1.im+z2.im;
     return temp;
 }
+struct zespolone pomnoz(struct zespolone z1, struct zespolone z2)
+{
+    struct zespolone iloczyn;
+    iloczyn.re=((z1.re*z2.re)+(z1.im*z2.im));
+    iloczyn.im=((z1.im*z2.re)-(z1.re*z2.im));
+    return iloczyn;
+};
+
+struct zespolone podziel(struct zespolone z1, struct zespolone z2)
+{
+    struct zespolone iloraz;
+    iloraz.re=((z1.re*z2.re)+(z1.im*z2.im))/((pow(z2.re, 2)+pow(z2.im, 2)));
+    iloraz.im=((z1.im*z2.re)-(z1.re*z2.im))/((pow(z2.re, 2)+pow(z2.im, 2)));
+    return iloraz;
+};
 
 int main()
 {
     struct zespolone liczba1={3,7};
     struct zespolone liczba2={8,2};
     struct zespolone suma=dodaj(liczba1, liczba2);
-    printf("%f+%fi",suma.re,suma.im);
+    struct zespolone iloczyn=pomnoz(liczba1, liczba2);
+    struct zespolone iloraz=podziel(liczba1, liczba2);
+    printf("%f+%fi\n",suma.re, suma.im);
+    printf("%f+%fi",iloraz.re, iloraz.im);
     return 0;
 }
